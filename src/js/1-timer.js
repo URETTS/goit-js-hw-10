c92ee7045dcd6777315162712a2d22aa31b01082
URETTS <yurka.b2000@gmail.com>
2024-12-10 19:47:39 +0400

    task 1

---
+++ src/js/1-timer.js
@@ -0,0 +1,98 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const datetimePicker = document.querySelector("#datetime-picker");
+const startButton = document.querySelector("[data-start]");
+const daysSpan = document.querySelector("[data-days]");
+const hoursSpan = document.querySelector("[data-hours]");
+const minutesSpan = document.querySelector("[data-minutes]");
+const secondsSpan = document.querySelector("[data-seconds]");
+
+let userSelectedDate = null;
+let timerId = null;
+
+startButton.disabled = true;
+
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
+
+function updateTimerInterface({ days, hours, minutes, seconds }) {
+  daysSpan.textContent = addLeadingZero(days);
+  hoursSpan.textContent = addLeadingZero(hours);
+  minutesSpan.textContent = addLeadingZero(minutes);
+  secondsSpan.textContent = addLeadingZero(seconds);
+}
+
+function toggleControls(isActive) {
+  datetimePicker.disabled = !isActive;
+  startButton.disabled = !isActive;
+}
+
+// Параметри для flatpickr
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      const selectedDate = selectedDates[0];
+    if (!selectedDate || selectedDate <= new Date()) {
+      iziToast.error({
+        title: "Error",
+        message: "Please choose a date in the future",
+      });
+      startButton.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr(datetimePicker, options);
+
+// Обробник натискання на кнопку Start
+startButton.addEventListener("click", () => {
+  if (!userSelectedDate) return;
+
+    toggleControls(false);
+    startButton.disabled = true;
+    
+
+  timerId = setInterval(() => {
+    const currentTime = new Date();
+    const timeDifference = userSelectedDate - currentTime;
+
+    if (timeDifference <= 0) {
+      clearInterval(timerId);
+      toggleControls(true);
+      iziToast.success({
+        title: "Completed",
+        message: "Countdown finished!",
+      });
+      return;
+    }
+
+    const timeLeft = convertMs(timeDifference);
+    updateTimerInterface(timeLeft);
+  }, 1000);
+});
+
+// Number of milliseconds per unit of time
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
